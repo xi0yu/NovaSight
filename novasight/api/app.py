996cfeff5ea6fd75ad3a2ed7435989277c161eb5
xi0yu <xoyomiyo@gmail.com>
2026-07-08 10:02:49 +0800

    Add v1 control config API

--- novasight/api/app.py
+++ novasight/api/app.py
@@ -19,6 +19,7 @@ from novasight.runtime import ControlFrameCsvRecorder, ControlFrameParquetRecord
 from novasight.systemd import SystemdNotifier, watchdog_interval_from_env
 
 from .routes_capture import router as capture_router
+from .routes_control import router as control_router
 from .routes_device import router as device_router
 from .routes_executors import router as executors_router
 from .routes_health import router as health_router
@@ -126,6 +127,7 @@ def create_app(
     app.include_router(health_router)
     app.include_router(device_router)
     app.include_router(capture_router)
+    app.include_router(control_router)
     app.include_router(runtime_router)
     app.include_router(status_router)
     app.include_router(models_router)
