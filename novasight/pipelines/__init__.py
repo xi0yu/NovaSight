5139681530e4bbc800525c2749aacaacc55e0a5e
xi0yu <xoyomiyo@gmail.com>
2026-07-08 08:59:59 +0800

    Add DeepStream detection probe slot

--- novasight/pipelines/__init__.py
+++ novasight/pipelines/__init__.py
@@ -6,13 +6,25 @@ from .gst_validator import (
     is_nvmm_buffer,
     validate_nvmm_pipeline,
 )
+from .probes import (
+    DetectionProbeConfig,
+    DetectionSlot,
+    TimestampConverter,
+    detection_probe,
+    iter_detection_batches_from_batch_meta,
+)
 
 __all__ = [
+    "DetectionProbeConfig",
+    "DetectionSlot",
     "DeepStreamPipeline",
     "GstValidationResult",
     "PadProbeCallback",
+    "TimestampConverter",
     "build_capture_nvmm_pipeline",
     "buffer_memory_types",
+    "detection_probe",
+    "iter_detection_batches_from_batch_meta",
     "is_nvmm_buffer",
     "validate_nvmm_pipeline",
 ]
